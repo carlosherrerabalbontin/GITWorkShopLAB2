5ac42ff5aec3a6d4dc65669eb7cfbcf2f03bdb0c
carlosherrerabalbontin <c.herrerabalbontin@yahoo.es>
2021-06-29 12:55:57 +0200

    add console

--- MyConsoleApp/Module1.vb
+++ MyConsoleApp/Module1.vb
@@ -4,6 +4,9 @@
         Console.WriteLine("este es mi programa")
         Console.ReadLine()
         '''commentario de texto
+
+        Console.WriteLine("este es mi programa despues del comentario de texto")
+
     End Sub
 
 End Module
